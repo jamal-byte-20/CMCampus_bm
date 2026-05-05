bc86637b11fab07d3b6036ce00567d754991895a
jamal-byte-20 <kerroujamal54@gmail.com>
2026-05-05 14:07:18 +0100

    create header and footer and home page

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar(){
-    return <h1>nav</h1>
+    return <header className="bg-white shadow-sm sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+
+        {/* Logo */}
+        <h1 className="text-2xl font-bold text-cmc-cyan-dark">
+          CMCampus
+        </h1>
+
+        {/* Links */}
+        <nav className="hidden md:flex gap-8 text-gray-700 font-medium">
+          <Link to="/" className="hover:text-cmc-cyan-dark">Home</Link>
+          <Link to="/about" className="hover:text-cmc-cyan-dark">About</Link>
+          <Link to="/courses" className="hover:text-cmc-cyan-dark">Courses</Link>
+          <Link to="/filieres" className="hover:text-cmc-cyan-dark">Filieres</Link>
+          <Link to="/blogs" className="hover:text-cmc-cyan-dark">Blogs</Link>
+        </nav>
+
+        {/* Button */}
+        <Link
+          to="/login"
+          className="bg-cmc-cyan text-white px-5 py-2 rounded-lg hover:bg-cmc-cyan-dark transition"
+        >
+          Login
+        </Link>
+      </div>
+    </header>
 }
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,17 +1,39 @@
 export default function Home() {
   return (
-    <>
-      <button className="btn-primary">
-  Découvrir les filières
-</button>
+    <section className="bg-gray-50 min-h-screen flex items-center">
+      <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-2 gap-10 items-center">
 
-<h2 className="section-title">
-  Bienvenue au CMC
-</h2>
+        {/* Text */}
+        <div>
+          <h1 className="text-4xl md:text-5xl font-bold text-gray-900 leading-tight">
+            Build Your Future at CMC Beni Mellal
+          </h1>
 
-<p className="section-desc">
-  Découvrez les formations, les pôles et les opportunités du campus.
-</p>
-    </>
+          <p className="mt-4 text-gray-600">
+            A professional training center offering practical programs to help stagiaires gain real-world skills and prepare for the job market.
+          </p>
+
+          <div className="mt-6 flex gap-4">
+            <button className="bg-cmc-cyan text-white px-6 py-3 rounded-lg hover:bg-blue-700">
+              Explore Filières
+            </button>
+
+            <button className="border px-6 py-3 rounded-lg">
+              Contact Us
+            </button>
+          </div>
+        </div>
+
+        {/* Image */}
+        <div className="">
+          <img
+            src="../static/pictures/cmc-hero.png"
+            alt="education"
+            className="w-100 object-cover"
+          />
+        </div>
+
+      </div>
+    </section>
   );
 }
