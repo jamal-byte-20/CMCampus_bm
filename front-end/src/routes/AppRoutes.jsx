2c0767a4e8803d2ac9bf9a8ee0247df8df449295
jamal-byte-20 <kerroujamal54@gmail.com>
2026-05-06 19:49:13 +0100

    create courses page and filieres page

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -2,11 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Blogs from "../pages/Blogs"
 import About from "../pages/About";
-import Lessons from "../pages/Lessons";
 
 import Filieres from "../pages/Filieres";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Courses from "../pages/Courses";
 
 export default function AppRoutes() {
   return (
@@ -16,7 +16,7 @@ export default function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/blogs" element={<Blogs />} />
       <Route path="/about" element={<About />} />
-      <Route path="/lessons" element={<Lessons />} />
+      <Route path="/courses" element={<Courses />} />
       <Route path="/filieres" element={<Filieres />} />
     </Routes>
     <Footer/>
